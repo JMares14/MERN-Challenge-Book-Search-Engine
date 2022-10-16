4dfa6408654301967bf1093d1b66d202c3617173
JM <georgemares14@yahoo.com>
2022-10-15 21:53:17 -0700

    trying to fix heroku deployments

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:/BookSearch', {
+mongoose.connect(process.env.MONGODB_URI || 'https://data.mongodb-api.com/app/data-dnris/endpoint/data/v1', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
